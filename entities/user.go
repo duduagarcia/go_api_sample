0a36110b28431de09d23b0fac8833501533a7680
Jocemar Nicolodi Junior <jnicolodijunior@gmail.com>
2024-10-10 19:03:57 -0300

    user_correct_correct_afu

--- entities/user.go
+++ entities/user.go
@@ -1,13 +1,8 @@
 package entities
 
-import (
-	"gorm.io/gorm"
-)
-
-// UserModel is the GORM model for the User entity, including database annotations.
-type UserModel struct {
-	gorm.Model        // Adds fields like ID, CreatedAt, UpdatedAt, and DeletedAt
-	Name       string `gorm:"size:100;not null"`        // Name with a size limit and "not null" constraint
-	Email      string `gorm:"size:100;unique;not null"` // Email must be unique and "not null"
-	Age        int    // Age of the user
+type User struct {
+	ID    uint   // The user's unique ID (this could be set in the repository layer)
+	Name  string // The user's name
+	Email string // The user's unique email
+	Age   int    // The user's age
 }
