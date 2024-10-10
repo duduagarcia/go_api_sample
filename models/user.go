b9554496d83241e5a0af2fc5400368ebc785ead7
Jocemar Nicolodi Junior <jnicolodijunior@gmail.com>
2024-10-10 18:51:19 -0300

    useringorm

---
+++ models/user.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+	ID    uint   // The user's unique ID (this could be set in the repository layer)
+	Name  string // The user's name
+	Email string // The user's unique email
+	Age   int    // The user's age
+}
