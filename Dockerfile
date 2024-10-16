0bee6a2bfd1c989ced54e1838b7661617ace55e0
Eduardo Amengual Garcia <103845062+duduagarcia@users.noreply.github.com>
2024-10-16 13:45:43 +0000

    feat: installated PG model for Gorm, Changed docker file, added pgAdmin in docker compose and created api connection with database

--- Dockerfile
+++ Dockerfile
@@ -1,40 +1,19 @@
-# Stage 1: Build the Go application
-FROM golang:1.21-alpine AS builder
+FROM golang:1.21-bullseye
+
+# Definir o diretório de trabalho
 WORKDIR /app
 
-# Install Git and build dependencies (such as GCC)
-RUN apk add --no-cache git gcc musl-dev
-
-# Copy go mod and sum files
-COPY go.mod go.sum ./
-
-RUN go mod download
-
-# Copy the source code
+# Copiar todo o projeto para o contêiner
 COPY . .
 
-
-# Set environment variables for CGO and target architecture
-ENV CGO_ENABLED=1 GOOS=linux
+# Instalar dependências
+RUN go mod download
 
 RUN go install github.com/swaggo/swag/cmd/swag@latest
 RUN swag init
 
-# Build the Go app for Linux with CGO enabled
-RUN go build -o main .
-
-
-# Stage 2: Copy the binary and run it in a smaller image
-FROM alpine:latest
-WORKDIR /app
-
-# Install build dependencies for CGO (SQLite support)
-RUN apk add --no-cache postgresql-client
-# Copy the built binary from the builder stage
-COPY --from=builder /app/main .
-
-# Expose the application port
+# Expor a porta que a aplicação vai usar
 EXPOSE 8080
 
-# Command to run the executable
-CMD ["./main"]
+# Comando para rodar a aplicação Go
+CMD ["go", "run", "main.go"]
--- configs/config.go
+++ configs/config.go
@@ -1,20 +1,47 @@
 package configs
 
 import (
-    "github.com/grupoG/csw24-grupoG-ticket-gin/models"
-    "log"
-    "gorm.io/driver/sqlite"
-    "gorm.io/gorm"
+	"fmt"
+	"log"
+
+	"github.com/grupoG/csw24-grupoG-ticket-gin/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func SetupDatabase() *gorm.DB {
-    db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+    // db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+    // if err != nil {
+    //     log.Fatal("Falha ao conectar ao banco de dados:", err)
+    // }
+
+    // // Migrar o schema
+    // db.AutoMigrate(&models.Sample{})
+
+    // return db
+
+    // Obter informações de conexão do ambiente
+    // host := os.Getenv("DB_HOST")
+    // user := os.Getenv("POSTGRES_USER")
+    // password := os.Getenv("POSTGRES_PASSWORD")
+    // dbname := os.Getenv("POSTGRES_DB")
+
+    host := "postgresdb"
+    user := "postgres"
+    password := "postgres"
+    dbname := "mydb"
+    port := "5432" // Porta padrão do Postgres
+
+    // String de conexão
+    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         log.Fatal("Falha ao conectar ao banco de dados:", err)
     }
 
     // Migrar o schema
-    db.AutoMigrate(&models.Sample{})
+    db.AutoMigrate(&models.Sample{}, &models.UserModel{})
 
     return db
 }
