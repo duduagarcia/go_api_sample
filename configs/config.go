3b5c98e77df59a3fb6c65edeb0656193831212a5
duduagarcia <eduardo.agarcia117@gmail.com>
2024-10-18 18:01:14 -0300

    feat: cleaning repo, letting only sample

--- configs/config.go
+++ configs/config.go
@@ -3,7 +3,6 @@ package configs
 import (
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/grupoG/csw24-grupoG-ticket-gin/models"
 	"gorm.io/driver/postgres"
@@ -30,12 +29,6 @@ func SetupDatabase() *gorm.DB {
 	// Migrar o schema
 	err = db.AutoMigrate(
 		&models.Sample{},
-		&models.Tenant{}, 
-		&models.User{}, 
-		&models.Event{}, 
-		&models.Ticket{}, 
-		&models.Transaction{}, 
-		&models.NotificationPreferences{},
 	)
 	if err != nil {
 		log.Fatal("Falha ao migrar o schema:", err)
@@ -54,40 +47,6 @@ func createInitialData(db *gorm.DB) {
 	sample2 := models.Sample{ID: 2, Name: "Sample B", Email: "Description B"}
 	db.Create(&sample1)
 	db.Create(&sample2)
-
-	// Criando Tenants
-	tenant1 := models.Tenant{Name: "Tenant A", ContactInfo: "contact@tenanta.com", SpecificConfigurations: "Config A"}
-	tenant2 := models.Tenant{Name: "Tenant B", ContactInfo: "contact@tenantb.com", SpecificConfigurations: "Config B"}
-	db.Create(&tenant1)
-	db.Create(&tenant2)
-
-	// Criando Users
-	user1 := models.User{Name: "Alice", Email: "alice@example.com", TenantID: tenant1.ID}
-	user2 := models.User{Name: "Bob", Email: "bob@example.com", TenantID: tenant2.ID}
-	db.Create(&user1)
-	db.Create(&user2)
-
-	// Criando Events
-	event1 := models.Event{TenantID: tenant1.ID, EventName: "Conference", Type: "Business", Location: "New York", EventDateTime: time.Now().AddDate(0, 1, 0)}
-	event2 := models.Event{TenantID: tenant2.ID, EventName: "Music Festival", Type: "Entertainment", Location: "Los Angeles", EventDateTime: time.Now().AddDate(0, 2, 0)}
-	db.Create(&event1)
-	db.Create(&event2)
-
-	// Criando Tickets
-	ticket1 := models.Ticket{EventID: event1.ID, TenantID: tenant1.ID, OriginalPrice: 100.00, SellerID: user1.ID, VerificationCode: "ABC123", Status: "Available"}
-	ticket2 := models.Ticket{EventID: event2.ID, TenantID: tenant2.ID, OriginalPrice: 150.00, SellerID: user2.ID, VerificationCode: "XYZ789", Status: "Available"}
-	db.Create(&ticket1)
-	db.Create(&ticket2)
-
-	// Criando Transactions
-	transaction1 := models.Transaction{TenantID: tenant1.ID, BuyerID: user2.ID, TicketID: ticket1.ID, SalePrice: 95.00, TransactionDate: time.Now(), TransactionStatus: "Completed"}
-	db.Create(&transaction1)
-
-	// Criando NotificationPreferences
-	preferences1 := models.NotificationPreferences{UserID: user1.ID, ReceiveEmails: true}
-	preferences2 := models.NotificationPreferences{UserID: user2.ID, ReceiveEmails: false}
-	db.Create(&preferences1)
-	db.Create(&preferences2)
 
 	fmt.Println("Dados iniciais criados com sucesso!")
 }
--- entities/user.go
+++
@@ -1,8 +0,0 @@
-package entities
-
-type User struct {
-	ID    uint   // The user's unique ID (this could be set in the repository layer)
-	Name  string // The user's name
-	Email string // The user's unique email
-	Age   int    // The user's age
-}
